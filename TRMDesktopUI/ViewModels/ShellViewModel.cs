1b075c6af8236cc214e8527004f19f743d2d0d46
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-06-18 23:13:51 -0400

    Add Event Aggregation in WPF
    Adds event and handle event to ShellViewModel so it close Login Form and open Sales Page

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -4,16 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using TRMDesktopUI.EventModels;
 
 namespace TRMDesktopUI.ViewModels
 {
-    public class ShellViewModel : Conductor<Object>
+    public class ShellViewModel : Conductor<Object>, IHandle<LogOnEvent>
     {
-        private LoginViewModel _loginVM;
-        public ShellViewModel(LoginViewModel loginVM) //constructor injection
+        private IEventAggregator _events;
+        private SalesViewModel _salesVM;
+        private SimpleContainer _container;
+        public ShellViewModel(LoginViewModel loginVM, IEventAggregator events,
+            SalesViewModel salesVM, SimpleContainer container) //constructor injection
         {
-            _loginVM = loginVM;
-            ActivateItem(_loginVM);
+            _events = events;
+            _salesVM = salesVM;
+            _container = container;
+
+            _events.Subscribe(this);
+
+            ActivateItem(_container.GetInstance<LoginViewModel>());
+
+        }
+
+        public void Handle(LogOnEvent message)
+        {
+            //because of the conductor, one form will be activate
+            //so login form will be close 
+            ActivateItem(_salesVM);
         }
     }
 }
