d412bc974685611d03b5aabe517f42fa2fa64462
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-06-21 19:40:17 -0400

    Add User Roles
    Add authorization by role to the endpoints.

---
+++ InsertInto-AspNetRoles.sql
@@ -0,0 +1,7 @@
+﻿
+
+
+insert into dbo.AspNetRoles (Id, [Name])
+values (NEWID(), 'Cashier')
+	, (NEWID(), 'Manager')
+	, (NEWID(), 'Admin')
--- TRMDataManager/Controllers/InventoryController.cs
+++ TRMDataManager/Controllers/InventoryController.cs
@@ -12,12 +12,15 @@ namespace TRMDataManager.Controllers
     [Authorize]
     public class InventoryController : ApiController
     {
+        [Authorize(Roles = "Manager,Admin")] //Manager (or) Admin can get the inventory info
         public List<InventoryModel> Get()
         {
             InventoryData data = new InventoryData();
             return data.GetInventory();
         }
-
+        //if you have Warehouse Worker and you want to make it join mission
+        //[Authorize(Roles = "WarehouseWorker")] //Admin (and) WarehouseWorker can add to the inventory
+        [Authorize(Roles = "Admin")]
         public void Post(InventoryModel item)
         {
             InventoryData data = new InventoryData();
