88e6e9cc6fbc11886e4982797972b321e75d9f0e
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-06-23 10:56:58 -0400

    Added New API (.NET Core) and Moved Over Functionality

---
+++ TRMApi/Controllers/InventoryController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class InventoryController : ControllerBase
+    {
+        [Authorize(Roles = "Manager,Admin")] //Manager (or) Admin can get the inventory info
+        public List<InventoryModel> Get()
+        {
+            InventoryData data = new InventoryData();
+            return data.GetInventory();
+        }
+        //if you have Warehouse Worker and you want to make it join mission
+        //[Authorize(Roles = "WarehouseWorker")] //Admin (and) WarehouseWorker can add to the inventory
+        [Authorize(Roles = "Admin")]
+        public void Post(InventoryModel item)
+        {
+            InventoryData data = new InventoryData();
+            data.SaveInventoryRecord(item);
+        }
+    }
+}
