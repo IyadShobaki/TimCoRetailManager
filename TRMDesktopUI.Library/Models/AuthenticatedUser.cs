4e990f6800c32553ef83773de0854de6ea7075c5
IyadShobaki <iyadshobaki82@gmail.com>
2020-08-03 13:21:36 -0400

    Fix Bad Namaspace
    Changed a namespace to be correct.

--- TRMDesktopUI.Library/Models/AuthenticatedUser.cs
+++ TRMDesktopUI.Library/Models/AuthenticatedUser.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TRMDesktopUI.Models
+namespace TRMDesktopUI.Library.Models
 {
     //the following class will enable us to get token from the api
     public class AuthenticatedUser
